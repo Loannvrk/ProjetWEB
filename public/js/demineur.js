6dc21d495118971e0248a048598bfcc08734a35e
MOUTYMBO <3800601@ppti-24-303-09.ufr-info-p6.jussieu.fr>
2019-10-21 12:31:29 +0200

    code demineur finit

--- public/js/demineur.js
+++ public/js/demineur.js
@@ -1,20 +1,22 @@
-var board;// Plateau
+var board=[];// Plateau contenant les éléments de jeu
 //(-1 : bombe)(0 : case vide)(x>0 : case proche de x bombes)
-var visibleBoard;// Plateau visible
+var boardVisible=[];// Plateau visible
 //(0 : case cachée)(1 : case contenant un numéro ou vide)(2 : case drapeau)(3 : case point d'interrogation)
-var htmlBoard;
-var gamediv = document.querySelector("#gamediv");
-const ratio = 0.2;
-var bombs;
+var boardHtml;
+var gamediv = document.querySelector("#gamedivD");// Ecran de jeu
+const ratio = 0.1;// Ratio de bombes/cases
+var bombs;// Nombre de bombes
 // Init function
 function initGame(size){
   var bombs = Math.round(size*size*ratio);
   // Mise en place du plateau et du plateau visible par l'utilisateur
   do{
     for(var i=0;i<size;i++){
+      board[i]=[];
+      boardVisible[i]=[];
       for(var j=0;j<size;j++){
-        visibleBoard[i][j]=0;
-        if(Math.random() < ratio && bombs > 0){
+        boardVisible[i][j]=0;
+        if(Math.random() < ratio){
           board[i][j]=-1;
           bombs--;
         }
@@ -27,25 +29,27 @@ function initGame(size){
   // Détection du nombre de bombes sur les cases adjacentes
   for(var i=0;i<size;i++){
     for(var j=0;j<size;j++){
-      if(board[i][j]!=-1){
+      if( board[i][j] != (-1) ){
         detectionBomb(i,j,size);
       }
     }
   }
 }
+
 // Compte le nombre de bombes sur les cases adjacentes à la case (x,y)
 function detectionBomb(x,y,size){
-  var counter;
+  var nbBomb=0;
   // Comptage du nombre de bombes sur cases adjacentes
-  for(var i=x-1;i<x+1;i++){
-    for(var j=y-1;j<y+1;j++){
-      if( (i >= 0 && i < size) && (j >= 0 && j < size) && tab[i][j] == -1){
-        counter++;
+  for(var i=x-1;i<=x+1;i++){
+    for(var j=y-1;j<=y+1;j++){
+      if( (i >= 0 && i < size) && (j >= 0 && j < size) && (board[i][j] == -1)){
+        nbBomb++;
       }
     }
   }
-  tab[i][j]=counter;
+  board[x][y]=nbBomb;
 }
+
 // Initialise le plateau de jeu en HTMl
 function initInnerHTML(size) {
   /*HTMLElement * int -> HTMLElement[]
@@ -54,22 +58,23 @@ function initInnerHTML(size) {
   for (var i=0; i<size; i++) {
     innerhtml += "<tr>";
     for (var j=0; j<size; j++) {
-      innerhtml += "<th id=\"cell\"></th>";
+      innerhtml += "<th class=\'cell\'>";
+      innerhtml += board[i][j];
+      innerhtml +="</th>";
     }
     innerhtml += "</tr>";
   }
   gamediv.innerHTML = innerhtml;
+  return document.querySelectorAll("tr");
 }
-
-htmlBoard = document.querySelectorAll("tr");
 
 function initClickListeners(){
   /*HTMLElement[] -> void
   creates cells' click cells*/
   var cell;
-  for (var i=0; i<htmlBoard.length; i++) {
-    cell = htmlBoard[i].childNodes;
-    for(var j=0; i<cell.length;j++){
+  for (var i=0; i<boardHtml.length; i++) {
+    cell = boardHtml[i].childNodes;
+    for(var j=0; j<cell.length;j++){
       cell[j].addEventListener("click",getPlayOnCell(i,j));
     }
   }
@@ -81,34 +86,34 @@ function getPlayOnCell(x,y){
 }
 
 function playOnCell(x,y,button){
-  if(button==0 && visibleBoard[x][y]==0){
+  if(button==0 && boardVisible[x][y]==0){
     switch (board[x][y]){
       case -1 :
-        htmlBoard[x].childNodes[y].setAttribute("id","theOne");
+        boardHtml[x].childNodes[y].setAttribute("id","theOne");
         lost();
         break;
       case 0 :
         caseVide(x,y);
         break;
       default :
-        visibleBoard[x][y]=1;
-        htmlBoard[x].childNodes[y].innerText(board[x][y]);
-        htmlBoard[x].childNodes[y].setAttribute("id","visible");
+        boardVisible[x][y]=1;
+        boardHtml[x].childNodes[y].innerText = board[x][y];
+        boardHtml[x].childNodes[y].setAttribute("id","visible");
     }
   }
   else if(button==2){
-    switch(visibleBoard[x][y]){
+    switch(boardVisible[x][y]){
       case 0 :
-        visibleBoard[x][y]==2;
-        htmlBoard[x].childNodes[y].setAttribute("id","flag");
+        boardVisible[x][y]==2;
+        boardHtml[x].childNodes[y].setAttribute("id","flag");
         break;
       case 2 :
-        visibleBoard[x][y]==3;
-        htmlBoard[x].childNodes[y].setAttribute("id","questionMark");
+        boardVisible[x][y]==3;
+        boardHtml[x].childNodes[y].setAttribute("id","questionMark");
         break;
       case 3 :
-        visibleBoard[x][y]=0;
-        htmlBoard[x].childNodes[y].setAttribute("id","");
+        boardVisible[x][y]=0;
+        boardHtml[x].childNodes[y].setAttribute("id","");
         break;
     }
   }
@@ -116,17 +121,21 @@ function playOnCell(x,y,button){
 }
 // Dévoile les cases adjacentes à la case vide (x,y)
 function caseVide(x,y){
-  for(var i=x-1;i<x+1;i++){
-    for(var j=y-1;j<y+1;j++){
-      switch(board[i][j]){
-        case 0 :
-          caseVide(i,j);
-          break;
-        default :
-          htmlBoard[x].childNodes[y].innerText(board[x][y]);
+  boardHtml[x].childNodes[y].setAttribute("id","visible");
+  var limit = board.length;
+  for(var i = x-1 ; i <= x+1; i++){
+    for(var j = y-1; j <= y+1; j++){
+        if( (i >= 0 && i < limit) && (j >= 0 && j < limit) && (boardVisible[i][j] != 1) ){
+          boardVisible[i][j]=1;
+          boardHtml[i].childNodes[j].setAttribute("id","visible");
+          switch(board[i][j]){
+            case 0 :
+              caseVide(i,j);
+              break;
+            default :
+              boardHtml[i].childNodes[j].innerText = board[i][j];
+        }
       }
-      htmlBoard[x].childNodes[y].setAttribute("id","visible");
-      visibleBoard[x][y]=1;
     }
   }
 }
@@ -136,25 +145,25 @@ function lost(){
   for(var i=0;i<board.length;i++){
     for(var j=0;j<board[i].length;j++){
      if(board[i][j]==-1){
-      htmlBoard[i].childNodes[j].setAttribute("class","bomb");// Affiche toutes les bombes
+      boardHtml[i].childNodes[j].setAttribute("class","bomb");// Affiche toutes les bombes
      }
-     visibleBoard[i][j]=1; // Empêche de continuer à jouer
+     boardVisible[i][j]=1; // Empêche de continuer à jouer
     }
   }
   gamediv.setAttribute("id","perdu");
 }
 
 function partieGagnee(){
-  for(var i=0;i<visibleBoard.length;i++){
-    for(var j=0;j<visibleBoard[i].length;j++){
-      if(visibleBoard[i][j]==0 && board[i][j]!=-1){
+  for(var i=0;i<boardVisible.length;i++){
+    for(var j=0;j<boardVisible[i].length;j++){
+      if(boardVisible[i][j]==0 && board[i][j]!=-1){
         return ;
       }
     }
   }
-  for(var i=0;i<visibleBoard.length;i++){
-    for(var j=0;j<visibleBoard[i].length;j++){
-      visibleBoard[i][j]==1;// Bloque le plateau
+  for(var i=0;i<boardVisible.length;i++){
+    for(var j=0;j<boardVisible[i].length;j++){
+      boardVisible[i][j]==1;// Bloque le plateau
     }
   }
   gamediv.setAttribute("id","gagne");
@@ -162,8 +171,9 @@ function partieGagnee(){
 
 function Init(size) {
   /*int -> HTMLElement[]*/
-  initInnerHTML(size);                      // créer le tableau aléatoire
+  initGame(size);
+  boardHtml = initInnerHTML(size);                       // créer le tableau aléatoire
   initClickListeners();                      // rendre les cellules du tableau clickables
-  initGame(size);                               // créer les tableaux de jeux
+                              // créer les tableaux de jeux
 }
-Init(10);
+Init(4);
